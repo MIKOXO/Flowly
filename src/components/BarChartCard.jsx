9b537f52827bbbe488dd112047bf55221df19a7f
MIKOXO <mikermount19@yahoo.com>
2025-11-15 17:51:51 +0300

    Dynamic functionality added

--- src/components/BarChartCard.jsx
+++ src/components/BarChartCard.jsx
@@ -9,28 +9,22 @@ import {
   ResponsiveContainer,
 } from "recharts";
 
-const BarChartCard = () => {
-  const data = [
-    { name: "Completed", value: 8 },
-    { name: "Active", value: 5 },
-    { name: "Pending", value: 2 },
-  ];
-
+const BarChartCard = ({ data }) => {
   const isDark = document.documentElement.classList.contains("dark");
-  const textColor = isDark ? "" : " hsl(220, 15%, 65%)";
+  const textColor = isDark ? "hsl(220, 15%, 65%)" : "hsl(220, 15%, 65%)";
 
   return (
-    <div className="bg-bg-LightCard dark:bg-DarkCard border border-LightBorder dark:border-DarkBorder rounded-2xl shadow-md p-6">
+    <div className="bg-white dark:bg-DarkCard border border-LightBorder dark:border-DarkBorder rounded-2xl shadow-md p-6">
       <h3 className="text-lg font-semibold mb-4">Total Tasks Overview</h3>
       <div className="h-36">
         <ResponsiveContainer width="100%" height="100%">
           <BarChart data={data} barSize={35}>
-            <CartesianGrid strokeDasharray="" stroke="" />
+            <CartesianGrid />
             <XAxis dataKey="name" tick={{ fill: textColor }} />
-            <YAxis tick={{ fill: textColor }} />
+            <YAxis allowDecimals={false} tick={{ fill: textColor }} />
             <Tooltip />
             <Bar
-              dataKey="value"
+              dataKey="count"
               fill="hsl(225, 70%, 55%)"
               radius={[6, 6, 0, 0]}
             />
--- src/components/StatCard.jsx
+++ src/components/StatCard.jsx
@@ -7,7 +7,7 @@ const StatCard = ({ title, value, total, color }) => {
   const percentage = Math.round((value / total) * 100);
 
   return (
-    <motion.div className="bg-LightCard dark:bg-DarkCard border border-LightBorder dark:border-DarkBorder rounded-2xl shadow-md p-6 flex flex-col space-y-5 cursor-pointer">
+    <motion.div className="bg-white dark:bg-DarkCard border border-LightBorder dark:border-DarkBorder rounded-2xl shadow-md p-6 flex flex-col space-y-5 cursor-pointer">
       <h3 className="text-lg font-semibold">{title}</h3>
       <div className="text-4xl font-bold">{value}</div>
       <ProgressBar percentage={percentage} color={color} />
