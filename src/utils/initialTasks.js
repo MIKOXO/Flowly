a6599dd34daf5455d74fabff68861ef2eac3f489
MIKOXO <mikermount19@yahoo.com>
2025-11-15 17:50:56 +0300

    Name change

---
+++ src/utils/initialTasks.js
@@ -0,0 +1,14 @@
+export const initialData = {
+  todo: {
+    title: "To-Do",
+    items: [],
+  },
+  inprogress: {
+    title: "In-Progress",
+    items: [],
+  },
+  completed: {
+    title: "Completed",
+    items: [],
+  },
+};
